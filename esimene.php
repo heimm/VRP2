cce7f8cc06ac58a5f3af39bd92290990b606089f
unknown <merlenhe@KLASS2-28.intra.itcollege.ee>
2016-04-09 14:07:09 +0300

    Minu lahendused

---
+++ esimene.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Esimene HTML!</title>
+	<link rel="stylesheet" type="text/css" href="stiil.css">
+	<script src="myscript.js"></script>
+	</head>
+	
+</head>
+<body onload="startTime()">
+
+<div id="txt"></div>
+
+<h1>HTML pealkiri</h1>
+<p>Võrgurakendused I</p>
+<?php
+	//See siin on PHP kood...
+	include("counter.php");
+	include("mysql.php");
+	?>
+  <p></p><img src="pilt.jpg" alt="HTML pilt" /></p>
+<p> See on pilt sõnast HTML :)</p>
+
+
+</body>
+</html>
---
+++ mysql.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+  if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+	$proxy = $_SERVER['HTTP_X_FORWARDED_FOR']; //testimata; vb ei tööta
+  else
+    $proxy = "";
+
+  $host="localhost";
+  $user="test";
+  $pass="t3st3r123";
+  $db="test";
+  $muutuja= "INSERT INTO `test`.`merlenhe_lehekylg_kylastajad` (
+ `kellaaeg` ,
+`ip_v4` ,
+`ip_v4_proxy` 
+)
+VALUES (
+ NOW(), '{$_SERVER['REMOTE_ADDR']}', '{$proxy}'
+);
+";
+  $connection = mysqli_connect($host, $user, $pass, $db) or die("ei saa �hendust mootoriga");
+  mysqli_query($connection, $muutuja) or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
+  
